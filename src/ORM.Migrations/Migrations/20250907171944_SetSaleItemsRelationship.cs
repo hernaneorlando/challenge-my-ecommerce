7b05b811b1b3508a030af29f631b7f29840da2d4
Hernene Cunha <hernaneorlando@hotmail.com>
2025-09-07 14:21:25 -0300

    Add mew migration related to Sale and Sale Items relationship, and Sale Data changing type

---
+++ src/ORM.Migrations/Migrations/20250907171944_SetSaleItemsRelationship.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ORM.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetSaleItemsRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SalesItems_Sales_SaleId",
+                table: "SalesItems");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Date",
+                table: "Sales",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SalesItems_Sales_SaleId",
+                table: "SalesItems",
+                column: "SaleId",
+                principalTable: "Sales",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SalesItems_Sales_SaleId",
+                table: "SalesItems");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Sales",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SalesItems_Sales_SaleId",
+                table: "SalesItems",
+                column: "SaleId",
+                principalTable: "Sales",
+                principalColumn: "Id");
+        }
+    }
+}
